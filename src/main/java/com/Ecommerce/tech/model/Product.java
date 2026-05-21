54eca7fa01f7afd0b82528c82b48afa2b4dc0347
joaoferraz4986-dot <joaoferraz4986@gmail.com>
2026-05-21 06:04:25 -0300

    feat: Create a product entity

---
+++ src/main/java/com/Ecommerce/tech/model/Product.java
@@ -0,0 +1,56 @@
+package com.Ecommerce.tech.model;
+
+import co.elastic.clients.util.DateTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table( name = "product")
+public class Product {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private long id;
+
+    @Column( length = 200, nullable = false )
+    private String name;
+
+    @Column( length = 5000 )
+    private String description;
+
+    @Column( precision = 10, scale = 2, nullable = false )
+    private BigDecimal price;
+
+    private int quantity = 0;
+
+    @Column( length = 100, unique = true )
+    private String sku;
+
+    @Enumerated( EnumType.STRING )
+    private UserStatus userStatus = UserStatus.ENABLED;
+
+    @Column( name = "created_at", updatable = false, nullable = false )
+    private OffsetDateTime createdAt = OffsetDateTime.now(ZoneOffset.UTC);
+
+    @ManyToOne( fetch = FetchType.LAZY )
+    @JoinColumn( name = "seller_id" )
+    @Column( nullable = false )
+    private Seller seller;
+
+    @ManyToOne( fetch = FetchType.LAZY )
+    @JoinColumn( name = "category_id" )
+    @Column( nullable = false )
+    private Category category;
+}
